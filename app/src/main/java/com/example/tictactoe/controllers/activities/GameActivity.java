8fe53743e7e4785794aceaadffc86cdf92765519
Mehdi <mnezamipoor@gmail.com>
2020-07-24 12:08:07 +0430

    Initial commit : packaging app and design layout of tic tac toe

---
+++ app/src/main/java/com/example/tictactoe/controllers/activities/GameActivity.java
@@ -0,0 +1,52 @@
+package com.example.tictactoe.controllers.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.tictactoe.R;
+import com.example.tictactoe.controllers.fragments.FourInARowFragment;
+import com.example.tictactoe.controllers.fragments.TicTacToeFragment;
+
+public class GameActivity extends AppCompatActivity {
+
+    private Button mButtonTicTacToe;
+    private Button mButtonFourInARow;
+    
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+        findViews();
+        final FragmentManager fragmentManager = getSupportFragmentManager();
+        mButtonTicTacToe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                fragmentManager.beginTransaction()
+                        .replace(R.id.container_fragment, new TicTacToeFragment())
+                        .commit();
+            }
+        });
+
+        mButtonFourInARow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                fragmentManager.beginTransaction()
+                        .replace(R.id.container_fragment, new FourInARowFragment())
+                        .commit();
+            }
+        });
+    }
+
+
+    private void findViews() {
+        mButtonTicTacToe = findViewById(R.id.button_tic_tac_toe);
+        mButtonFourInARow = findViewById(R.id.button_four_in_a_row);
+    }
+
+
+}
