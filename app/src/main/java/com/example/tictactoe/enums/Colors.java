404902e1195386120ebc57460386dac618187505
Mehdi <mnezamipoor@gmail.com>
2020-07-28 09:26:37 +0430

    Design layout of fragment_four_in_a_row.xml

---
+++ app/src/main/java/com/example/tictactoe/enums/Colors.java
@@ -0,0 +1,5 @@
+package com.example.tictactoe.enums;
+
+public enum Colors {
+    RED, BLUE
+}
---
+++ app/src/main/java/com/example/tictactoe/enums/Players.java
@@ -0,0 +1,5 @@
+package com.example.tictactoe.enums;
+
+public enum Players {
+    PLAYER_1, PLAYER_2;
+}
